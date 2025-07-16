9d9631fd86f00b468951a87dae8e4e9821104e76
hridoyratna9 <140900301+hridoyratna9@users.noreply.github.com>
2025-07-16 08:33:06 +0600

    python basics

---
+++ day-1/hello.py
@@ -0,0 +1,56 @@
+# # # print("hello World!!!")
+# # # print("Hridoy Ratno!!")
+# # # num=23
+# # # print(num)
+# # # name ='''Hridoy Ratno'''
+# # # age=24
+# # # a=None
+# # # print("My name is : ",name)
+# # # print("My age is : ",age)
+# # # print(type(name))
+# # # print(type(age))
+# # # print(type(a))
+
+
+# # a,b=1,2.0
+# # sum=a+b
+# # print(sum)
+
+# # """#error
+# # a,b=1,"3.0"
+# # Sum=a+b
+# # print(Sum)
+# # """
+# # #Type Casting
+# # a,b=1,"3.0"
+# # c=float(b)              #integer value nila int function use korta hoba
+# # Sum1=a+c
+# # print(Sum1)
+
+
+
+# # #Right
+# # a,b="1","3.0"
+# # Sum=a+b
+# # print(Sum)
+
+# # name=input("Enter any name : ")   
+# # print("My name is ",name)
+
+# # name1=input()   
+# # print(name1)
+# # multiple=float(input("Enter any number : "))   
+# # print("The value of multiple is ",multiple)
+# # divider=int(input("Enter any name : "))   
+# # print("The value of divider is ",divider)
+
+# #practice : input square side and print area
+# length=float(input("Enter the length of Square : "))
+# area= length*length
+# print("The Area of Square is ",area)
+
+#exponential operator(**)
+# h=int(input())
+# i=int(input())
+# Exponential= h**i
+# print("The valaue of Exponential equation is ",Exponential)
